cf4eb5eed61a0bd5d20834f89320f19d9c8d4aeb
Pablo Martinez-Abrego Gonzalez <pmarti22@student10.cse.nd.edu>
2020-11-03 19:48:19 -0500

    add scripts

---
+++ finalproject/client/scripts/main.js
@@ -0,0 +1,22 @@
+// main.js for Meal Reccomendation
+// Lukasz and Pablo
+
+console.log('entered main.js')
+
+
+// Clear information on screen between searches
+function clear(args) {
+
+  
+}
+
+function findMeal(args) {
+
+
+}
+
+
+function performSearch() {
+
+
+}
