c3e33ba757a723419f0e9383c3f27f10829e5718
Lukasz Matwiejczyk <lmatwiej@nd.edu>
2020-11-13 01:54:55 +0000

    Cleaned test_reset_endpoint.py

--- finalproject/webserver/test_reset_endpoint.py
+++ finalproject/webserver/test_reset_endpoint.py
@@ -4,53 +4,71 @@ import json
 
 class TestReset(unittest.TestCase):
 
+    # Define url endpoints needed for the unittests
     SITE_URL = 'http://student10.cse.nd.edu:51077' # replace with your port id
     print("Testing for server: " + SITE_URL)
     RESET_URL = SITE_URL + '/reset/'
     FOOD_URL = SITE_URL + '/food_name/'
 
+    # Test PUT_KEY
     def test_put_reset_index(self):
+
+        # Delete the database
         m = {}
         r = requests.delete(self.FOOD_URL, data = json.dumps(m))
+
+        # Now call put and proceed to see if database still there
+        # If database restored after deletion, put request succeeded
         r = requests.put(self.RESET_URL)
-
         resp = json.loads(r.content.decode('utf-8'))
-
         self.assertEqual(resp['result'], 'success')
+
+        # To check if database restored, issue get request
         r = requests.get(self.FOOD_URL)
         resp = json.loads(r.content.decode('utf-8'))
 
+        # Populate a testfood dictionary with id = 200 information
+        # Dictionary will not be empty if put was successful
         testfood = {}
         foods = resp['food']
         for food in foods:
-            if food['id'] == 1020:
+            if food['id'] == 200:
                 testfood = food
 
-        self.assertEqual(testfood['name'], 'Cheese, fontina')
-        self.assertEqual(testfood['group'], 'Dairy and Egg Products')
-        self.assertEqual(testfood['kcal'], 389.0)
-        self.assertEqual(testfood['protein'], 25.6)
-        self.assertEqual(testfood['fat'], 31.14)
-        self.assertEqual(testfood['carb'], 1.55)
+        # Test if the food is correct
+        self.assertEqual(testfood['name'], 'TURKEY BACON')
+        self.assertEqual(testfood['group'], 'Sausages and Luncheon Meats')
+        self.assertEqual(testfood['kcal'], 368)
+        self.assertEqual(testfood['protein'], 29.5)
+        self.assertEqual(testfood['fat'], 25.87)
+        self.assertEqual(testfood['carb'], 4.24)
 
+    # Test for PUT_KEY
     def test_put_reset_key(self):
 
-        food_id = 1020
+        # First delete entry under food id 200
+        food_id = 200
         m = {}
         r = requests.delete(self.FOOD_URL + str(food_id), data = json.dumps(m))
-        r = requests.put(self.RESET_URL + str(food_id))
-
         resp = json.loads(r.content.decode('utf-8'))
         self.assertEqual(resp['result'], 'success')
 
+
+        # After deleting, put the food_id back
+        r = requests.put(self.RESET_URL + str(food_id))
+        resp = json.loads(r.content.decode('utf-8'))
+        self.assertEqual(resp['result'], 'success')
+
+        # To check that it's back and put worked, issue a get request
+        # Check to see if correct information is there
         r = requests.get(self.FOOD_URL + str(food_id))
         resp = json.loads(r.content.decode('utf-8'))
-        self.assertEqual(resp['name'], 'Cheese, fontina')
-        self.assertEqual(resp['group'], 'Dairy and Egg Products')
-        self.assertEqual(resp['kcal'], 389.0)
-        self.assertEqual(resp['prot'], 25.6)
-        self.assertEqual(resp['fat'], 31.14)
-        self.assertEqual(resp['carb'], 1.55)
+        self.assertEqual(testfood['name'], 'TURKEY BACON')
+        self.assertEqual(testfood['group'], 'Sausages and Luncheon Meats')
+        self.assertEqual(testfood['kcal'], 368)
+        self.assertEqual(testfood['protein'], 29.5)
+        self.assertEqual(testfood['fat'], 25.87)
+        self.assertEqual(testfood['carb'], 4.24)
 
 if __name__ == "__main__":
     unittest.main()
