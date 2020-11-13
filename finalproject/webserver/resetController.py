e0a4aedeb8b1e09ec8f57224af0cda39eaba1047
Lukasz Matwiejczyk <lmatwiej@nd.edu>
2020-11-13 03:28:15 +0000

    Quickly Add reset of favorites log

--- finalproject/webserver/resetController.py
+++ finalproject/webserver/resetController.py
@@ -1,18 +1,24 @@
 import cherrypy
 import re, json
 from food_library import _food_database
+from favorites_library import _favorites_log
 
 class ResetController(object):
 
     # Initialize the database in the constructor
-    def __init__(self, mdb=None):
+    def __init__(self, mdb=None, log=None):
         if mdb is None:
             self.mdb = _food_database()
         else:
             self.mdb = mdb
 
+        if log is None:
+            self.log = _favorites_log()
+        else:
+            self.log = log
+
     def PUT_INDEX(self):
-	# when PUT request comes in to /reset/ endpoint, then the movie database is reloaded'''
+	# when PUT request comes in to /reset/ endpoint, then the food and favorites database is reloaded'''
         output = {'result':'success'}
 
         # data = json.loads(cherrypy.request.body.read().decode())
@@ -20,10 +26,13 @@ class ResetController(object):
         self.mdb.__init__()
         self.mdb.load_food('food_data.dat')
 
+        self.log.__init__()
+        self.log.load_food('food_data.dat')
+
         return json.dumps(output)
 
     def PUT_KEY(self, food_id):
-	 # when PUT request comes in for /reset/food_id endpoint, then that food_id is reloaded and updated in mdb'''
+	 # when PUT request comes in for /reset/food_id endpoint, then that food_id is reloaded and updated in mdb and log'''
         output = {'result':'success'}
         fid = int(food_id)
 
