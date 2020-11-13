7b18e3a78a8d885a146fb173aeff7c7635afebc3
Lukasz Matwiejczyk <lmatwiej@nd.edu>
2020-11-13 01:05:21 +0000

    Add commentary to index calls

--- finalproject/webserver/foodController.py
+++ finalproject/webserver/foodController.py
@@ -5,16 +5,18 @@ from food_library import _food_database
 
 class FoodController(object):
 
+        # Initialize by calling the API constructor and defining a self.fdb database
         def __init__(self, mdb=None):
                 if mdb is None:
                         self.mdb = _food_database()
                 else:
                         self.mdb = mdb
 
+                # Load the data
                 self.mdb.load_food('food_data.dat')
 
         def GET_KEY(self, food_id):
-		#'''when GET request for /movies/movie_id comes in, then we respond with json string'''
+		'''when GET request for /movies/movie_id comes in, then we respond with json string'''
                 output = {'result':'success'}
                 food_id = int(food_id)
 
@@ -78,25 +80,35 @@ class FoodController(object):
 
 
         def GET_INDEX(self):
-		#'''when GET request for /movies/ comes in, we respond with all the movie information in a json str'''
+		'''when GET request for /food_name/ comes in, we respond with all the food information in a json str'''
+
                 output = {'result':'success'}
                 output['food'] = []
 
                 try:
-                    for fid in self.mdb.get_foods():
-                        food = self.mdb.get_food(fid)
-                        dfood = {'id':fid, 'name':food[0],'group':food[1],'kcal':food[2],'protein':food[3],'fat':food[4],'carb':food[5]}
-                        output['food'].append(dfood)
+                        # Use get_foods() to retrieve all the food ids
+                        for fid in self.mdb.get_foods():
+                                # Use each food id to get that food's specific information
+                                food = self.mdb.get_food(fid)
+
+                                # Create dictionary with the information
+                                # Append it to output
+                                dfood = {'id':fid, 'name':food[0],'group':food[1],'kcal':food[2],'protein':food[3],'fat':food[4],'carb':food[5]}
+                                output['food'].append(dfood)
                 except Exception as ex:
-                    output['result'] = 'error'
-                    output['message'] = str(ex)
+                        output['result'] = 'error'
+                        output['message'] = str(ex)
 
                 return json.dumps(output)
 
         def POST_INDEX(self):
-		#'''when POST for /movies/ comes in, we take title and genres from body of request, and respond with the new movie_id and more'''
+		'''when POST for /food_name comes in, we take request body with new food information and add it to the database'''
                 output = {'result':'success'}
+
+                # Load the data from request
                 data = json.loads(cherrypy.request.body.read().decode('utf-8'))
+
+                # Construct list of food info from request body
                 food = list()
                 food.append(data['name'])
                 food.append(data['group'])
@@ -104,12 +116,14 @@ class FoodController(object):
                 food.append(data['prot'])
                 food.append(data['fat'])
                 food.append(data['carb'])
+
+                # Iterate through id's to set food_id as the highest
                 food_id = 1
                 for fid in self.mdb.get_foods():
                     food_id = fid
-
                 food_id = food_id + 1
 
+                # Utilize API function set_food to add the entry
                 self.mdb.set_food(food_id, food)
                 if self.mdb.get_food(food_id) is None:
                     output['result'] = 'error'
@@ -120,32 +134,40 @@ class FoodController(object):
                 return json.dumps(output)
 
         def DELETE_INDEX(self):
-            output = {'result':'success'}
+                '''when DELETE for /food_name/ comes in, we clear every entry from the database'''
+                output = {'result':'success'}
 
-            self.mdb.food_name.clear()
-            if self.mdb.food_name:
-                output['result'] = 'error'
-                output['message'] = 'Unable to clear all names'
-            self.mdb.food_group.clear()
-            if self.mdb.food_group:
-                output['result'] = 'error'
-                output['message'] = 'Unable to clear all groups'
-            self.mdb.food_kcal.clear()
-            if self.mdb.food_kcal:
-                output['result'] = 'error'
-                output['message'] = 'Unable to clear all kcal'
-            self.mdb.food_protein.clear()
-            if self.mdb.food_protein:
-                output['result'] = 'error'
-                output['message'] = 'Unable to clear all proteins'
-            self.mdb.food_fat.clear()
-            if self.mdb.food_fat:
-                output['result'] = 'error'
-                output['message'] = 'Unable to clear all fat'
-            self.mdb.food_carb.clear()
-            if self.mdb.food_carb:
-                output['result'] = 'error'
-                output['message'] = 'Unable to clear all carb'
+                # Clear each dictioary in the database and check if clear
+                # If not cleared, set output result to error
+                self.mdb.food_name.clear()
+                if self.mdb.food_name:
+                        output['result'] = 'error'
+                        output['message'] = 'Unable to clear all names'
+                        
+                self.mdb.food_group.clear()
+                if self.mdb.food_group:
+                        output['result'] = 'error'
+                        output['message'] = 'Unable to clear all groups'
+
+                self.mdb.food_kcal.clear()
+                if self.mdb.food_kcal:
+                        output['result'] = 'error'
+                        output['message'] = 'Unable to clear all kcal'
+
+                self.mdb.food_protein.clear()
+                if self.mdb.food_protein:
+                        output['result'] = 'error'
+                        output['message'] = 'Unable to clear all proteins'
+
+                self.mdb.food_fat.clear()
+                if self.mdb.food_fat:
+                        output['result'] = 'error'
+                        output['message'] = 'Unable to clear all fat'
+
+                self.mdb.food_carb.clear()
+                if self.mdb.food_carb:
+                        output['result'] = 'error'
+                        output['message'] = 'Unable to clear all carb'
 
 
-            return json.dumps(output)
+                return json.dumps(output)
