1cfd3f4883a0714f752e148b1ef8fffc1ce5595a
Lukasz Matwiejczyk <lmatwiej@nd.edu>
2020-11-13 01:38:59 +0000

    Cleaned test_food_key.py

--- finalproject/webserver/test_food_key.py
+++ finalproject/webserver/test_food_key.py
@@ -4,16 +4,18 @@ import json
 
 class TestFood(unittest.TestCase):
 
+    # Initialize endpoints to be used
     SITE_URL = 'http://student10.cse.nd.edu:51077' # replace with your port number and
     print("testing for server: " + SITE_URL)
     FOOD_URL = SITE_URL + '/food_name/'
     RESET_URL = SITE_URL + '/reset/'
 
+    # Helper function that resets data at start of individual tests
     def reset_data(self):
         m = {}
         r = requests.put(self.RESET_URL, data = json.dumps(m))
 
-
+    # Returns true if valid JSON
     def is_json(self, resp):
         try:
             json.loads(resp)
@@ -21,33 +23,33 @@ class TestFood(unittest.TestCase):
         except ValueError:
             return False
 
+    # Test GET_KEY
     def test_food_get_key(self):
         self.reset_data()
-        food_id = 1003
+
+        # Initialize food id and url endpoint
+        food_id = 424
         r = requests.get(self.FOOD_URL + str(food_id))
+
+        # Test if response is valid JSON 
         self.assertTrue(self.is_json(r.content.decode('utf-8')))
         resp = json.loads(r.content.decode('utf-8'))
-        self.assertEqual(resp['name'], 'Butter oil, anhydrous')
-        self.assertEqual(resp['group'], 'Dairy and Egg Products')
-        self.assertEqual(resp['kcal'], 876.0)
-        self.assertEqual(resp['prot'], 0.28)
-        self.assertEqual(resp['fat'], 99.48)
-        self.assertEqual(resp['carb'], 0)
 
+        # Test if the correct food information returned (under id = 424)
+        self.assertEqual(resp['name'], 'ACORNS')
+        self.assertEqual(resp['group'], 'Nut and Seed Products')
+        self.assertEqual(resp['kcal'], 387)
+        self.assertEqual(resp['prot'], 6.15)
+        self.assertEqual(resp['fat'], 23.86)
+        self.assertEqual(resp['carb'], 40.75)
+
+    # Test PUT_KEY
     def test_food_put_key(self):
         self.reset_data()
-        food_id = 1004
 
-        r = requests.get(self.FOOD_URL + str(food_id))
-        self.assertTrue(self.is_json(r.content.decode('utf-8')))
-        resp = json.loads(r.content.decode('utf-8'))
-        self.assertEqual(resp['name'], 'Cheese, blue')
-        self.assertEqual(resp['group'], 'Dairy and Egg Products')
-        self.assertEqual(resp['kcal'], 353.0)
-        self.assertEqual(resp['prot'], 21.4)
-        self.assertEqual(resp['fat'], 28.74)
-        self.assertEqual(resp['carb'], 2.34)
+        # Attempt to modify id = 424, which contains information on acorns
 
+        # Define info for a new food entry
         m = {}
         m['name'] = 'Gerber'
         m['group'] = 'Baby Foods'
@@ -55,14 +57,19 @@ class TestFood(unittest.TestCase):
         m['prot'] = '2'
         m['fat'] = '3'
         m['carb'] = '8'
+
+        # Issue put request to food_id endpoint with the new information
         r = requests.put(self.FOOD_URL + str(food_id), data = json.dumps(m))
         self.assertTrue(self.is_json(r.content.decode('utf-8')))
         resp = json.loads(r.content.decode('utf-8'))
         self.assertEqual(resp['result'], 'success')
 
+        # See if put request worked: if new data is now present at id = 424
         r = requests.get(self.FOOD_URL + str(food_id))
         self.assertTrue(self.is_json(r.content.decode('utf-8')))
         resp = json.loads(r.content.decode('utf-8'))
+
+        # Unittests for each field of info under id = 424; should match the above
         self.assertEqual(resp['name'], 'Gerber')
         self.assertEqual(resp['group'], 'Baby Foods')
         self.assertEqual(resp['kcal'], '66')
@@ -70,16 +77,22 @@ class TestFood(unittest.TestCase):
         self.assertEqual(resp['fat'], '3')
         self.assertEqual(resp['carb'], '8')
 
+    # Test for DELETE_KEY
     def test_food_delete_key(self):
         self.reset_data()
-        food_id = 1008
+        
+        # Specify a valid food to delete
+        food_id = 600
 
+        # Make a delete request and load response
         m = {}
-        r = requests.delete(self.FOOD_URL + str(food_id), data = json.dumps(m))
+        r = requests.delete(self.FOOD_URL + str(food_id), data = json.dumps(m)
         self.assertTrue(self.is_json(r.content.decode('utf-8')))
         resp = json.loads(r.content.decode('utf-8'))
         self.assertEqual(resp['result'], 'success')
 
+        # Attempt to get that food_id
+        # Response should be error because food_id not present after deletion
         r = requests.get(self.FOOD_URL + str(food_id))
         self.assertTrue(self.is_json(r.content.decode('utf-8')))
         resp = json.loads(r.content.decode('utf-8'))
